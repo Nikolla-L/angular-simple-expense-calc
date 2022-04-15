cb90bbede257f81943b72313576bde8af5d9d7d1
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-04-16 00:18:18 +0400

    some changes

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.module';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,11 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
-  @Input() isIncome: boolean = true;
+  @Input() item: BudgetItem | undefined;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onXButtonClick = () => {
+    this.xButtonClick.emit();
+  }
 }
